c4452dae956ae24f220dd181fca722a6f55a1228
Muntasir Md. Nafis <46222806+nafis2508@users.noreply.github.com>
2021-10-24 00:45:38 +0600

    tuple properties

---
+++ tuples.py
@@ -0,0 +1,12 @@
+students = (
+    ("Nafis",23),
+    "Nehal",
+    "Jubair",
+)
+#tuples are not immutable
+#the values assigned in tuples cannot be changed
+#slicing is also possible
+print("The first index of the tuple is: ",students[0])
+print("The full tuple is : ",students)
+print("Slicing Implemented :",students[0:])
+
